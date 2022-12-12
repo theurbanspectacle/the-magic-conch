f69dfb5e6aa3a21612d3e28bff576828a4087339
Genesis (Oliver) G <107947533+theurbanspectacle@users.noreply.github.com>
2022-12-11 18:49:47 -0800

    array of questions changed colors

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
-// const conchQuestions = [
-//     "Maybe Someday.",
-//     "No.",
-//     "Yes.",
-//     ""
-// ]
+ const conchQuestions = [
+    "Maybe someday.",
+    "No.",
+    "Yes.",
+    "I don't think so.",
+    "Neither",
+    "[Mockingly] NoOoo",
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "You may rely on it.",
+    "Most likely.",
+    "Reply hazy, try again.",
+    "Try asking again.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+ ];
 
 // function conchQuestions () {
 //     if {
@@ -15,14 +28,11 @@
 
 // Link functions to generate a question
 // Add animation effects for visiblility of conch GIF to pop up AFTER button has been pressed.
-// Add a loading screen.
-// ready(function() {
-//     $(loader-wrapper).fadeOut(slow);
-//    });
-// $("document").ready($(".loader-wrapper").fadeOut("slow"));
 
 
+// Loading Page 
 $(document).ready(function(){
     console.log("hello");
     $(".loader-wrapper").delay(2000).fadeOut("slow")
 });
+// End of Loading Page
