4413636cb80967259839946347ae2d3faec58699
Genesis (Oliver) G <107947533+theurbanspectacle@users.noreply.github.com>
2022-12-11 18:06:13 -0800

    files, jquery, bootstrap, loading screen

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// const conchQuestions = [
+//     "Maybe Someday.",
+//     "No.",
+//     "Yes.",
+//     ""
+// ]
+
+// function conchQuestions () {
+//     if {
+//         user
+//     } else {
+//         console.log("Please input a question.")
+//     }
+// }
+
+// Link functions to generate a question
+// Add animation effects for visiblility of conch GIF to pop up AFTER button has been pressed.
+// Add a loading screen.
+// ready(function() {
+//     $(loader-wrapper).fadeOut(slow);
+//    });
+// $("document").ready($(".loader-wrapper").fadeOut("slow"));
+
+$(window).on("load", function(){
+    $(".loader-wrapper").fadeOut("slow")
+});
